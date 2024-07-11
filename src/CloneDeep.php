cc90c51c28f88c2b8f4a90958fd432e9e34bd3c0
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-07-11 00:31:39 +0000

    First objects

---
+++ src/CloneDeep.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Danilocgsilva\EntityCloneDeep;
+
+use PDO;
+
+class CloneDeep
+{
+    private PDO $pdo;
+
+    public function getIdsByRelatedField(string $idValue, string $tableName)
+    {
+
+    }
+}
